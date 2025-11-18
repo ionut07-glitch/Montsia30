8a3e010aa21e3f267788325b11c1232451954153
AleixPla <aleixpla@iesmontsia.org>
2025-11-19 00:20:36 +0100

    Add files via upload

---
+++ Log in.js
@@ -0,0 +1,21 @@
+function Correu() {
+    let correu = document.getElementById("correu").value;
+    let domini = "@montsia30.net";
+
+    if (!correu.endsWith(domini)) {
+        alert("El domini del correu ha de ser " + domini);
+        return false;
+    }
+    return true;
+}
+
+function Contrasenya() {
+    let contrasenya = document.getElementById("contrasenya").value;
+    let contingutcontrasenya = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[^A-Za-z0-9]).+$/;
+
+    if (!contingutcontrasenya.test(contrasenya)) {
+        alert("La contrasenya ha de tindre 8 caràcters; una majúscula, una minúscula i un caràcter especial.");
+        return false;
+    }
+    return true;
+}
